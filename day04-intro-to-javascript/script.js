f3f52ffbbd5b13837c8536e0c0ce1b737c80cd16
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-06-15 09:08:52 -0700

    end of morning session

--- day04-intro-to-javascript/script.js
+++ day04-intro-to-javascript/script.js
@@ -66,3 +66,115 @@ const jobs = [
     postDate: "6 days ago",
   },
 ];
+
+const key = "title";
+
+// console.log(jobs[0][key]);
+// console.log(jobs[0].title);
+// console.log(key[1])
+
+// for (let i = 0; i < jobs.length; i++) {
+//   console.log(jobs[i].title)
+// }
+
+// for (let i = 0; i < key.length; i++) {
+//   console.log(key[i])
+// }
+
+// let jobLocationPreference = "Remote";
+
+// console.log(jobLocationPreference)
+// jobLocationPreference = "Los Angeles";
+// console.log(jobLocationPreference)
+
+// const compliments = [];
+// console.log(compliments)
+// compliments.push("You are very friendly!");
+// console.log(compliments)
+
+// checking truthiness / falsiness
+// !!
+// console.log(!!true); // true
+// console.log(!!false); // false
+// console.log(!!""); // false
+// console.log(!!0); // false
+// console.log(!!1); // true
+// console.log(!![]); // true
+// console.log(!!{}); // true
+// console.log(!!"hello"); // true
+// console.log(!!undefined); // false
+// console.log(!!null); // false
+// console.log(!!NaN); // false
+
+// console.log(true + 2); // 3
+// console.log(false + 2); // 2
+// // type coercion is happening here: true => 1, false => 0
+// console.log('1' + 2);
+// console.log(2 + '1');
+
+// // I want to print to the console:
+// // The {company} company is hiring a {job title} and offering {salary}
+// const firstJob = jobs[0];
+// console.log(`The ${firstJob.company} company is hiring a ${firstJob.title} and offering ${firstJob.salary}`);
+
+// using destructuring assignment
+
+// const firstJob = jobs[0];
+// const { company, title, salary } = firstJob;
+// console.log(
+//   `The ${company} company is hiring a ${title} and offering ${salary}`
+// );
+
+// const trip = ["Lancaster", "Lake Arrowhead"];
+// const [from, to] = trip;
+// console.log(from);
+// console.log(to);
+
+
+// function calculateAverageSalary(jobs) {
+//   let totalSalary = 0;
+//   for (let i = 0; i < jobs.length; i++) {
+//     let salaryRange = jobs[i].salary.split(" - "); // Split the salary range into an array
+//     let minSalary = parseInt(salaryRange[0].replace(/\D/g, "")); // Get the minimum salary as a number
+//     let maxSalary = parseInt(salaryRange[1].replace(/\D/g, "")); // Get the maximum salary as a number
+//     let averageSalary = (minSalary + maxSalary) / 2; // Calculate the average salary
+//     totalSalary += averageSalary; // Add it to the total
+//   }
+//   return totalSalary / jobs.length; // Return the average salary across all jobs
+// }
+
+// console.log(`Average salary: ${calculateAverageSalary(jobs)}k`);
+// we get problems because our salaries have different formats!
+
+// let globalVar = "I am global!"; // This variable is globally scoped
+
+// function checkScope() {
+//   let localVar = "I am local!"; // This variable is locally scoped
+//   console.log(globalVar); // We can access the global variable here
+//   console.log(localVar); // And the local variable is also accessible
+// }
+
+// checkScope();
+// console.log(globalVar);
+// console.log(localVar);
+
+jobs.forEach(job => console.log(job.title))
+const jobTitles = jobs.map(job => job.title)
+console.log(jobTitles);
+
+const remoteJobs = jobs.filter(job => {
+  return job.location.toLowerCase().includes("remote");
+})
+console.log(remoteJobs);
+
+// filter in pseudocode
+// function accepts callback as argument
+// function is called on an original array
+// create a new empty array for returning
+// iterate over every element in the original array
+// pass each element to the callback function as an argument
+// check if the return value is truthy
+// if it is add the element to the array for returning
+// if not, leave it out
+// when finished iterating through the original array
+// return array for returning
