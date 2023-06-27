dd676355a49bf9a914ace80949ba1372c2badad0
DakotaLMartinez <dakotaleemusic@gmail.com>
2023-06-27 07:38:29 -0700

    update starter

--- day11-react-forms-and-lifting-state/job-app-tracker-starter/src/JobCard.jsx
+++ day11-react-forms-and-lifting-state/job-app-tracker-starter/src/JobCard.jsx
@@ -3,6 +3,7 @@ import { PropTypes } from "prop-types";
 function JobCard({ job }) {
   // pull data from argument
   const {
+    id,
     image: { src, alt },
     company,
     title,
@@ -12,10 +13,10 @@ function JobCard({ job }) {
   } = job;
 
   return (
-    <div className="flex items-start gap-4 my-13">
+    <div data-testid="job-card" className="flex items-start gap-4 my-13">
       <img src={src} alt={alt} />
       <div>
-        <h2 className="text-xl font-bold relative -top-1.5">{title}</h2>
+        <h2 className="text-xl font-bold relative -top-1.5" data-testid={id}>{title}</h2>
         <p className="text-gray-400 italic mb-2">{company}</p>
         <ul className="text-sm">
           <li>{location}</li>
@@ -28,17 +29,19 @@ function JobCard({ job }) {
 }
 
 JobCard.propTypes = {
-  job: {
-    image: {
+  job: PropTypes.shape({
+    image: PropTypes.shape({
       src: PropTypes.string,
       alt: PropTypes.string,
-    },
+    }),
     company: PropTypes.string,
     title: PropTypes.string,
     salary: PropTypes.string,
     location: PropTypes.string,
     postDate: PropTypes.string,
-  },
+  }),
 };
+
+console.log(typeof PropTypes.string)
 
 export default JobCard;
